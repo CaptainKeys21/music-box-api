8cf472c3217bfdadf89e845e67eb5dc25e28d5f1
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-06-19 00:06:25 -0300

    rename files

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,10 @@
 import { Options, Sequelize } from 'sequelize';
 import databaseConfig from '../configs/database' ;
-import Article from '../models/Article';
-import Author from '../models/Author';
-import ContentManager from '../models/ContentManager';
-import History from '../models/History';
-import User from '../models/User';
+import Article from '../models/Article.model';
+import Author from '../models/Author.model';
+import ContentManager from '../models/ContentManager.model';
+import History from '../models/History.model';
+import User from '../models/User.model';
 // * o arquivo index da database será por onde os models realizaram a conexão com a base de dados e associações entre tabelas
 
 const models = [User, Author, ContentManager, History, Article]; // * Toda vez que um model for criado, insira no array
