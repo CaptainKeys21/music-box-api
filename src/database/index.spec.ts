9e9d1af16e40040497fc3ebcc266b6de16bf2b71
captainKeys21 <captainkeys21@gmail.com>
2022-06-19 15:23:52 -0300

    add jest

---
+++ src/database/index.spec.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const sequelize = require('./index');
+describe('Database connection', () => {
+  test('is Connected', async () => {
+    expect.assertions(1);
+    try {
+      await sequelize.authenticate();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+});
+
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
 import app from './app';
-import { sequelize } from './database';
 
 app.listen(3000);
-
-async function test() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-
-test();
